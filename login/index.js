94dcc81fc863da2cd39753e082240d9db73065f5
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-10-08 20:18:29 -0300

    fix: use formData

--- login/index.js
+++ login/index.js
@@ -1,13 +1,12 @@
 function validateCredentials(event) {
   event.preventDefault();
-  console.log('clicked');
-  const email = document.querySelector('#email').value;
-  const password = document.querySelector('#password').value;
+  const formData = new FormData(loginForm);
+  const email = formData.get('email');
+  const password = formData.get('password');
 
   const expectedEmail = 'teste@gmail.com';
   const expectedPassword = '12345';
-
-  if (email == expectedEmail && password == expectedPassword) {
+  if (email === expectedEmail && password === expectedPassword) {
     alert('Login bem sucedido!');
 
     window.location.href = '/platforms/psn';
